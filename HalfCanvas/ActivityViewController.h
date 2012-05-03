b491940289b9d5add9ef6a37c78381d9c446c438
Ryan <ryeguy_24@yahoo.com>
2012-05-03 08:17:56 -0400

    Adding Activity View Controller

---
+++ HalfCanvas/ActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityViewController.h
+//  HalfCanvas
+//
+//  Created by Ryan Hittner on 4/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityViewController : UITableViewController
+
+@end
